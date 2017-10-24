c0909a3d0437ef801a63ce0a8214e782e20d5938
Lawrence Chan <lawrencepad@hotmail.com>
2017-10-24 11:48:07 +0800

    Fixed issue #1

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/classes/User.java
@@ -0,0 +1,21 @@
+package com.example.bot.spring.classes;
+
+@Slf4j
+
+public class User {
+	
+	public long userID;
+	
+	public void setInterests(String[] Categories) {
+		
+	}
+	
+	public void inputWeight(int weight) {
+		
+	}
+	
+	public void inputMeal(String food) {
+		
+	}
+	
+}
