8d6597cedb39afb9add976eefd7e592763d856ba
Woo Chan Lee <woochanlee@Woos-MacBook-Pro.local>
2017-10-30 23:03:28 +0800

    Added Menu entity and repository

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/tables/Menu.java
@@ -0,0 +1,38 @@
+package com.example.bot.spring.tables;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Menu{
+	@Id
+	private long userID;
+	private long[] foodIdArray;
+
+	public Menu(long uid, long numberOfFood) {
+		userID = uid;
+		foodIdArray = new long[numberOfFood];
+	}
+
+	public long getUserID() {
+		return userID;
+	}
+
+	public long[] getFoodIdArray() {
+		return foodIdArray;
+	}
+
+	public void setUserID(long id) {
+		userID = id;
+	}
+
+	public void setFoodIdArray(long[] id) {
+		for(int i; i<id.length; i++) {
+			foodIdArray[i] = id[i];
+		}
+	}
+}
