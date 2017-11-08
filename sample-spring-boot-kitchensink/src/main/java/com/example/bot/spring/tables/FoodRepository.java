1399643c3ef0b02e95c277b7ab1e0e11f535724c
Lawrence Chan <lawrencepad@hotmail.com>
2017-11-08 20:49:43 +0800

    no message

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/tables/FoodRepository.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/tables/FoodRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.bot.spring.tables.Food;
 
 public interface FoodRepository extends CrudRepository<Food, Long> {
-	Food findById(final long foodID);
+	Food findById(Long foodID);
 }
