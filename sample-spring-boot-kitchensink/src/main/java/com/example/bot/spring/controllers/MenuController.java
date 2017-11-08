514ab9f8f2ca76312ffb582046ecadf054b0f20b
Lawrence Chan <lawrencepad@hotmail.com>
2017-11-09 02:32:29 +0800

    no message

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/controllers/MenuController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/controllers/MenuController.java
@@ -154,13 +154,11 @@ public class MenuController{
 			if(ml.getUserID().equals(userID)) { 
 	        		Date threeDaysAgo = new Date(System.currentTimeMillis()-(3*24*60*60*1000));
 	        		Date mealTime = new Date(ml.getTime().getTime());
+	        		System.out.println(threeDaysAgo.toString());
+	        		System.out.println(mealTime.toString());
+	        		System.out.println(mealTime.after(threeDaysAgo));
 	        		if(mealTime.after(threeDaysAgo)) {
-	        			System.out.println("HERE");
-	        			Set<Long> mealIds = new HashSet<Long>();
-	        			mealIds = generateFoodIDs(ml.getFood());
-	        			System.out.println("Copy");
-	        			System.out.println(mealIds.size());
-	        			foodIds.addAll(mealIds);
+	        			foodIds.addAll(generateFoodIDs(ml.getFood()));
 	        			System.out.println("TRY: ");
 	        			System.out.println(foodIds.size());
 	        		}
