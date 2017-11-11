4aa23c7d2d644c1fc3bb3c92addb17ded5e03028
Lawrence Chan <lawrencepad@hotmail.com>
2017-11-12 00:09:00 +0800

    Debugging

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/controllers/MenuController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/controllers/MenuController.java
@@ -157,10 +157,12 @@ public class MenuController{
     		if(scores[i][0]!=null && !scores[i][0].isEmpty()) {
             	String[] items = scores[i][0].split(",", -1);
             	finalScore[i] -= items.length;
+            	System.out.println("meals:" + items.length);
     		}
     		if(scores[i][1]!=null && !scores[i][1].isEmpty()) {
             	String[] items = scores[i][1].split(",", -1);
             	finalScore[i] += items.length;
+            	System.out.println("interests:" + items.length);
     		}
     	}
 		return finalScore;
