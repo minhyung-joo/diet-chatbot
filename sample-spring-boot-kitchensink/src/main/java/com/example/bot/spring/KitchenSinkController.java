dcd18870a3e5c3b23fcb4ea4e2a4b3749d28a61c
Woo Chan Lee <woochanlee@Woos-MacBook-Pro.local>
2017-11-18 15:33:12 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -504,7 +504,7 @@ public class KitchenSinkController {
 			                        		   new MessageAction("Dairy/Egg products", "Dairy and Egg Products"),
 			                        		   new MessageAction("Spices/Herbs", "Spices and Herbs"),
 			                        		   new MessageAction("Baby food", "Baby Foods"),
-			                        		   new MessageAction("Fats/Oils", "Fats and Oils")
+			                        		   new MessageAction("Fats/Oils", "Fats and Oils"),
 			                        		   new MessageAction("Poultry products", "Poultry Products"),
 			                        		   new MessageAction("Soups/Sauces/Gravies", "Soups, Sauces, and Gravies"),
 			                        		   new MessageAction("Sausages", "Sausages and Luncheon Meats"),
