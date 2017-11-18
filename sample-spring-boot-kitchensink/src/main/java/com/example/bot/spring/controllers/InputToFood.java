c9244c84566ebc2228c6f0ac1a0af2cf628855c3
minhyung-joo <mjoo@connect.ust.hk>
2017-11-18 18:55:04 +0800

    add jacoco and basic testing

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/controllers/InputToFood.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/controllers/InputToFood.java
@@ -37,13 +37,21 @@ public class InputToFood {
 	@Autowired
 	private MenuController menuController;
 
-    public @ResponseBody String readFromText(@RequestParam String userId, @RequestParam String text) {
+    public String readFromText(String userId, String text) {
 		menuController.setUserID(userId);
 		menuController.setMenu(text);
     	return menuController.pickFood();
     }
+    
+//    public String readFromText(String userId, String text) {
+//    	return "";
+//    }
 
     public String readFromJSON(String url) {
+    	if (url == null) {
+    		return "Invalid input";
+    	}
+    	
     	try {
     		RestTemplate restTemplate = new RestTemplate();
         	Menu[] menuList = restTemplate.getForObject(url, Menu[].class);
@@ -71,6 +79,10 @@ public class InputToFood {
     }
 
     public String readFromJPEG(DownloadedContent jpeg) {
+    	if (jpeg == null || jpeg.getPath() == null || jpeg.getUri() == null) {
+    		return "Invalid input";
+    	}
+    	
     	String menu = "";
     	RestTemplate restTemplate = new RestTemplate();
     	String apiKey = "AIzaSyCrPOUDlYLaAQLAXbFSiRgb16OSikBooP8";
@@ -110,8 +122,7 @@ public class InputToFood {
     	}
     }
     
-    @GetMapping(path="/getfooddetails")
-    public @ResponseBody String getFoodDetails(@RequestParam String food) {
+    public String getFoodDetails(String food) {
     		String resultFood = "You have entered " + food + "\n";
     		String[] splitFood = food.split("\\s+");
     		
--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/InputToFoodTest.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/InputToFoodTest.java
@@ -6,7 +6,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.File;
 import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +29,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.autoconfigure.orm.jpa.*;
 
 import com.google.common.io.ByteStreams;
 
@@ -43,28 +48,60 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 import com.example.bot.spring.controllers.InputToFood;
-import com.example.bot.spring.controllers.MenuController;
 import com.example.bot.spring.tables.FoodRepository;
+import com.example.bot.spring.KitchenSinkController.DownloadedContent;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { KitchenSinkTester.class, SQLDatabaseEngine.class })
 public class InputToFoodTest {
-//	@Autowired
-//	private InputToFood inputToFood;
+	private InputToFood inputToFood = new InputToFood();
 	
 	@Test
 	public void testReadFromJSON() throws Exception {
-//		String nullStr = null;
-//		String emptyStr = "";
-//		String invalidUrl = "htt:asdf1234.com";
-//		
-//		String nullResult = inputToFood.readFromJSON(nullStr);
-//		assertEquals(nullResult, "Failed to load URL.");
-//		
-//		String emptyResult = inputToFood.readFromJSON(emptyStr);
-//		assertEquals(emptyResult, "Failed to load URL.");
-//		
-//		String invalidResult = inputToFood.readFromJSON(invalidUrl);
-//		assertEquals(invalidResult, "Failed to load URL.");
+		String nullStr = null;
+		String emptyStr = "";
+		String invalidUrl = "htt:asdf1234.com";
+		String jsonUrl = "https://randomjsonserver.herokuapp.com";
+		
+		String nullResult = inputToFood.readFromJSON(nullStr);
+		assertEquals(nullResult, "Invalid input");
+		
+		String emptyResult = inputToFood.readFromJSON(emptyStr);
+		assertEquals(emptyResult, "Failed to load URL.");
+		
+		String invalidResult = inputToFood.readFromJSON(invalidUrl);
+		assertEquals(invalidResult, "Failed to load URL.");
+		
+		String result = inputToFood.readFromJSON(jsonUrl);
+		assertEquals(result, "The Foods in each entree are as followed:\n" + 
+				"1. Pork, Bean curd, Rice, \n" + 
+				"2. Pork, Sweet and Sour Sauce, Pork, \n" + 
+				"3. Chili, Chicken, Rice, ");
+	}
+	
+	@Test
+	public void testReadFromJPEG() throws Exception {
+		DownloadedContent nullContent = null;
+		DownloadedContent invalidFormatContent = new DownloadedContent(null, "");
+		URI uri = getClass().getResource("/static/test/sample-menu2.jpg").toURI();
+		File file = new File(uri);
+		Path path = file.toPath();
+		DownloadedContent jpeg = new DownloadedContent(path, uri.toString());
+		
+		String nullResult = inputToFood.readFromJPEG(nullContent);
+		assertEquals(nullResult, "Invalid input");
+		
+		String invalidResult = inputToFood.readFromJPEG(invalidFormatContent);
+		assertEquals(nullResult, "Invalid input");
+		
+		String result = inputToFood.readFromJPEG(jpeg);
+		System.out.println(result);
+		assertEquals(result, "Spicy Bean curd with Minced Pork served with Rice\n" + 
+				"Sweet and Sour Pork served with Rice\n" + 
+				"Chili Chicken on Rice\n" + 
+				"Fried instance noodle with Luncheon Meat\n" + 
+				"35\n" + 
+				"36\n" + 
+				"28 F\n" + 
+				"40\n");
 	}
 }
