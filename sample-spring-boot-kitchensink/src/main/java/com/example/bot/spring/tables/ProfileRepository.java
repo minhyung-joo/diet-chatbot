5355c3fc0a77a929cb673f205a6193c5112b3e67
Esjay Hong <shongad@connect.ust.hk>
2017-11-18 12:38:47 +0800

    debug

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/tables/ProfileRepository.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/tables/ProfileRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.bot.spring.tables.Profile;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long>{
-	public Profile findByID(long id);
 	public Profile findByUserID(String userID);
 }
